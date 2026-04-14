ef3fa35a12607e9bc06a39b15a5b8009ff36096f
lsanchez12-jala <lucas.sanchez@jalasoft.com>
2026-04-14 15:24:52 -0400

    fix: deleted old db migrations file, and imports

--- src/main/java/product/management/Main.java
+++ src/main/java/product/management/Main.java
@@ -1,7 +1,6 @@
 package product.management;
 
 import org.glassfish.grizzly.http.server.HttpServer;
-import product.management.Infrastructure.Migrations.DatabaseMigrationManager;
 import product.management.config.DaggerServiceComponent;
 import product.management.config.ServiceComponent;
 
@@ -13,8 +12,6 @@ public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         final ServiceComponent component = DaggerServiceComponent.builder().build();
 
-        DatabaseMigrationManager dbManager = component.migrationManager();
-        dbManager.run();
 
         HttpServer server = component.httpServer();
         server.start();
